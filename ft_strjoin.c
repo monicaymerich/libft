8edb868e33582d47192f4d000596c26a12f08239
Monica Aymerich Bellmunt <maymeric@car6s2.42barcelona.com>
2024-01-31 15:00:32 +0100

    Strjoin OK

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: maymeric <maymeric@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:03:33 by maymeric          #+#    #+#             */
-/*   Updated: 2024/01/22 15:17:00 by maymeric         ###   ########.fr       */
+/*   Updated: 2024/01/31 15:00:04 by maymeric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = 0;
 	siz = ft_strlen(s1);
 	siz = siz + ft_strlen(s2);
-	result = (char *)malloc(i);
+	result = (char *)malloc(siz + 1);
+	if (!result)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		result[i] = s1[i];
@@ -41,11 +43,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (result);
 }
 /*
-int	main(int argc, char **argv)
+int	main(void)
 {
-	char *test;
+	const char	t1[] = "";
+	const char	t2[] = "";
+	char 		t;
+	char const	*s1 = t1;
+	char const	*s2 = t2;
+	char		*test = &t;
 
-	test = ft_strjoin(argv[1], argv[2]);
+	test = ft_strjoin(s1, s2);
 	printf("Result = %s\n", test);
 	return (0);
 }
