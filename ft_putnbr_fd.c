1eb48bc1b23b70d89776d3ab3afc17019fd0068c
monicaymerich <monicaymerich@gmail.com>
2024-02-02 12:45:42 +0100

    putnbr falta norminette

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,37 @@
+
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n = -n;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd((n/10), fd);
+			ft_putchar_fd(((n %10) + '0'), fd);
+		}
+		else
+			ft_putchar_fd((n + '0'), fd);
+	}
+}
+
+/*
+int	main(void)
+{
+	int	test = 9;
+	int	fd = 1;
+
+	ft_putnbr_fd(test, fd);
+	return (0);
+}
+*/
