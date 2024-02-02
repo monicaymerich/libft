48cacd52e8a0015c5435a178c868dacf7c4bffa7
Mònica <monica@MacBook-MacBook-Pro-de-Andreu.local>
2024-02-02 10:54:45 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
 int	ft_len(int n)
 {
 	int	len;
@@ -26,34 +25,21 @@ int	ft_len(int n)
 	return (len);
 }
 
-int	get_exp(int n)
-{
-	int	exp;
-
-	exp = 1;
-	while (n > 9)
-	{
-		n = n / 10;
-		exp = exp * 10;
-	}
-	printf("Exp = %d\n", exp);
-	return (exp);
-}
-
 char	*ft_itoa(int n)
 {
-	int		i;
 	int		len;
-	int		exp;
-	int		aux;
+	int	sign;
 	char	*num;
-
-	(void) exp;
-	(void) aux;
-	i = 0;
-	len = ft_len(n);
-	printf("len = %d\n", len);
-	num = malloc(sizeof(char) * len + 1);
+	
+	len = 0;
+	sign = 0;
+	if (n < 0)
+	{
+		sign = 1;
+		n = - n;
+	}
+	len = ft_len(n) + sign;
+	num = malloc(sizeof(char) * (len + 1));
 	if (!num)
 		return (NULL);
 	if (n == -2147483648)
@@ -61,46 +47,28 @@ char	*ft_itoa(int n)
 		num = "-2147483648";
 		return (num);
 	}
-	if (n < 0)
-		len++;
-
 	num[len] = '\0';
+	len--;
 	while(n)
 	{
 		num[len] = (n % 10) + '0';
 		n = n / 10;
-		printf("len = %d\nnum[len] = %c\n", len, num[len]);
 		len--;
 	}
-	/*
-	if (n < 0)
-	{	
-		num[i] = '-';
-		n = -n;
-		i++;
-	}
-	while (n)
-	{
-		exp = get_exp(n);
-		aux = n / exp;
-		num[i] = aux + '0';
-		n = n - aux * exp;
-		i++;
-	}
-	num[i] = '\0';*/
+	if (sign == 1)
+		num[len] = '-';	
 	return (num);
 }
 /*
 int	main(void)
 {
-    int		test;
-    char	result[20];
+	int		test = -23;
+	char	result[20];
 	char	*res;
 
 	res = result;
-    test = 1000;
-    res = ft_itoa(test);
-    printf("Resultat en char %s\n Hauria de ser %d\n", res, test);
-    return (0);
+	res = ft_itoa(test);
+	printf("Resultat en char %s\n Hauria de ser %d\n", res, test);
+	return (0);
 }
 */
