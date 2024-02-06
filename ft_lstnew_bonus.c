cc7dee1f089df1d2a422cff1329f1cbcecfe3b21
monicaymerich <monicaymerich@gmail.com>
2024-02-06 10:49:54 +0100

    lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maymeric <maymeric@student.42barcel>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/02 19:58:07 by maymeric          #+#    #+#             */
-/*   Updated: 2024/02/02 20:00:37 by maymeric         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 t_list	*ft_lstnew(void *content)
@@ -24,19 +12,3 @@ t_list	*ft_lstnew(void *content)
 	return (list);
 }
 
-/*
-int	main(void)
-{
-	t_list hola;
-	t_list	*punt;
-	char	test[] = "Hola";
-
-	punt = ft_lstnew(test);
-	printf("Contingut = %s\n", punt->content);
-	
-	hola.content = test;
-	printf("Contingut = %s\n", hola.content);
-
-	return (0);
-}
-*/
