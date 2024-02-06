4e202c3d36dfb5f2d73e499ce7e02445940af44c
monicaymerich <monicaymerich@gmail.com>
2024-02-06 10:51:48 +0100

    lstadd error

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -3,8 +3,10 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*aux;
+	t_list 	*new;
 
 	aux = lst;
+	new = aux;
 	if(!f)
 		return (NULL);
 	while(lst)
@@ -12,5 +14,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		aux = f(lst);
 		aux = aux->next;
 		lst = lst->next;
-	}	
+	}
+	return (new);	
 }
