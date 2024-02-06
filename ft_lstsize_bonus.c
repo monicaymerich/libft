945fc3341be0d15eff1aaa9c44bc89a56fb5a9cb
monicaymerich <monicaymerich@gmail.com>
2024-02-06 08:28:33 +0100

    lstsize

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -17,27 +17,28 @@ t_list	*ft_lstnew(void *content);
 int	ft_lstsize(t_list *lst)
 {
 	int		i;
-	t_list	*aux;
 
 	i = 0;
-	aux = lst;
-	while (aux->next != NULL)
+	while (lst != NULL)
+	{
+		lst = lst->next;
 		i++;
+	}
 	return (i);
 }
 /*
-int	main(void)
-{
-	int 	total;
-	t_list	*list;
-	t_list	*aux;
+   int	main(void)
+   {
+   int 	total;
+   t_list	*list;
+   t_list	*aux;
 
-	aux = list;
-	aux = ft_lstnew("Node1");
-	aux++;
-	aux = ft_lstnew("Node2");
-	total = ft_lstsize(list);
-	printf("Total nodos = %d\n", total);
-	return (0);
-}
-*/
+   aux = list;
+   aux = ft_lstnew("Node1");
+   aux++;
+   aux = ft_lstnew("Node2");
+   total = ft_lstsize(list);
+   printf("Total nodos = %d\n", total);
+   return (0);
+   }
+   */
